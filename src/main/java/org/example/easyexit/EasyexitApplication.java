be16a198fa1d6705e65246cf956ecd4ca056c604
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2024-01-28 19:09:45 +0530

    initial setup

---
+++ src/main/java/org/example/easyexit/EasyexitApplication.java
@@ -0,0 +1,13 @@
+package org.example.easyexit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyexitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyexitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/easyexit/EasyexitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.easyexit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyexitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
